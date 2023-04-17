756552a2ee2e6b380b3ab3bc16fc3fedb94cb28c
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-17 14:44:48 +0200

    style: add style and colors

--- censura.php
+++ censura.php
@@ -5,19 +5,26 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Censura Parola</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <h1>Censura una parola</h1>
-  <?php 
-    $paragrafo = $_POST['paragraph'];
-    $parolaCensurata = $_POST['censur'];
-    $paragrafoCensurato = str_ireplace($parolaCensurata, '***',$paragrafo);
-  ?>
-  <h3>Paragrafo completo:</h3>
-  <p><i><?php echo $paragrafo; ?></i></p>
-  <span>Lunghezza: <?php echo strlen($paragrafo); ?> caratteri</span>
-  <h3>Paragrafo censurato:</h3>
-  <p><i><?php echo $paragrafoCensurato; ?></i></p>
-  <span>Nuova lunghezza: <?php echo strlen($paragrafoCensurato); ?> caratteri</span>
+  <div class="centered">
+    <h1>Censura una parola</h1>
+    <?php 
+      $paragrafo = $_POST['paragraph'];
+      $parolaCensurata = $_POST['censur'];
+      $paragrafoCensurato = str_ireplace($parolaCensurata, '***',$paragrafo);
+    ?>
+    <div class="top">
+      <h3>Paragrafo completo:</h3>
+      <p><i><?php echo $paragrafo; ?></i></p>
+      <span>Lunghezza: <?php echo strlen($paragrafo); ?> caratteri</span>
+    </div>
+    <div class="bottom">
+      <h3>Paragrafo censurato:</h3>
+      <p><i><?php echo $paragrafoCensurato; ?></i></p>
+      <span>Nuova lunghezza: <?php echo strlen($paragrafoCensurato); ?> caratteri</span>
+    </div>
+  </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,15 +5,22 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Bad Words</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <h1>PHP Bad Words</h1>
-  <form action="censura.php" method="POST">
-    <label for="paragraph">Paragrafo da compilare:</label>
-    <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Inserisci una frase" required></textarea>
-    <label for="censur">Parola da censurare:</label>
-    <input type="text" name="censur" id="censur" placeholder="Inserisci una parola" required>
-    <button type="submit">Invia</button>
-  </form>
+  <div class="centered">
+    <h1>PHP Bad Words</h1>
+    <form action="censura.php" method="POST">
+      <div class="top">
+        <label for="paragraph">Paragrafo da compilare:</label>
+        <textarea name="paragraph" id="paragraph" cols="50" rows="6" placeholder="Inserisci una frase" required></textarea>
+      </div>
+      <div class="bottom">
+        <label for="censur">Parola da censurare:</label>
+        <input type="text" name="censur" id="censur" placeholder="Inserisci una parola" required>
+      </div>
+      <button type="submit">Invia</button>
+    </form>
+  </div>
 </body>
 </html>
